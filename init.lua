db385926e39f7c1df99a50a90d792999c06e17e8
suresh <sureshpradhana19@gmail.com>
2023-11-10 11:20:38 +0530

    lsp config pyright tsserver rustanalyzer

--- init.lua
+++ init.lua
@@ -402,9 +402,9 @@ end
 local servers = {
   -- clangd = {},
   -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
+   pyright = {},
+   rust_analyzer = {},
+   tsserver = {},
 
   lua_ls = {
     Lua = {
