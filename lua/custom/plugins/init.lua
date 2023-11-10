291b8bc17b74d178655a2181d644c136ea814c35
suresh <sureshpradhana19@gmail.com>
2023-11-10 10:49:36 +0530

    lsp config

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,27 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+
+return {
+  {'akinsho/toggleterm.nvim', version = "*", config = function()
+    require("toggleterm").setup{
+      open_mapping = [[<c-\>]],
+      size = 20,
+      direction = 'float'
+    }
+
+    function _G.set_terminal_keymaps()
+      local opts = {buffer = 0}
+      vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+      vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+      vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+      vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+      vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+      vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+      vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+    end
+
+    vim.cmd('autocmd! TermOpen * lua set_terminal_keymaps()')
+  end},
+}
