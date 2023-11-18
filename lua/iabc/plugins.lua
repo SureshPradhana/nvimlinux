58ec608b06e340b00e59e0f089e90a81f9800bd6
suresh <sureshpradhana19@gmail.com>
2023-11-18 11:06:00 +0530

    modular

---
+++ lua/iabc/plugins.lua
@@ -0,0 +1,150 @@
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+
+vim.opt.rtp:prepend(lazypath)
+require('lazy').setup({
+
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+  'tpope/vim-sleuth',
+  'github/copilot.vim',
+  'preservim/nerdtree',
+ 'voldikss/vim-floaterm',
+  'B4rc1/obsidian-companion.nvim',
+  'mfussenegger/nvim-dap',
+  { 'folke/which-key.nvim', opts = {} },
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    build = "cd app && npm install",
+    init = function() vim.g.mkdp_filetypes = { "markdown" } end,
+  },
+  
+    "goolord/alpha-nvim",
+
+  { -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'onedark',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+
+  { -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      { 'williamboman/mason.nvim', config = true },
+      'williamboman/mason-lspconfig.nvim',
+      'folke/neodev.nvim',
+    },
+  },
+
+  { -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+  },
+
+ { -- Adds git releated signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+
+
+  { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    opts = {
+      char = '┊',
+      show_trailing_blankline_indent = false,
+    },
+  },
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  -- Fuzzy Finder (files, lsp, etc)
+  { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
+
+
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make',
+    cond = function()
+      return vim.fn.executable 'make' == 1
+    end,
+  },
+
+  { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ":TSUpdate",
+  },
+  {'akinsho/toggleterm.nvim', version = "*", config = function()
+    require("toggleterm").setup{
+      open_mapping = [[<c-\>]],
+      size = 20,
+      direction = 'float'
+    }
+
+    function _G.set_terminal_keymaps()
+      local opts = {buffer = 0}
+      vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+      vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+      vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+      vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+      vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+      vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+      vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+    end
+
+    vim.cmd('autocmd! TermOpen * lua set_terminal_keymaps()')
+  end
+  },
+  --colroschemes
+  'tanvirtin/monokai.nvim',
+  { 
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'onedark'
+    end,
+  },
+  { 
+    "catppuccin/nvim", name = "catppuccin"
+  },
+  {'Yazeed1s/oh-lucy.nvim'},
+  'mhartington/oceanic-next',
+  'shaunsingh/nord.nvim',
+  'navarasu/onedark.nvim',
+  'RRethy/nvim-base16',
+  'kaicataldo/material.vim',
+  'nordtheme/vim',
+
+  -- custom plugins
+  -- { import = 'custom.plugins' },
+
+  {}})
+
+
---
+++ lua/iabc/set.lua
@@ -0,0 +1,48 @@
+
+--custom settings
+
+
+-- Set highlight on search
+vim.o.hlsearch = false
+vim.o.incsearch = true
+
+-- Make line numbers default
+vim.wo.number = true
+
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim.
+vim.o.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
+
+--font settings
+vim.g.WebDevIconsUnicodeByteOrderMarkerDefaultSymbol = ''
+vim.g.WebDevIconsUnicodeDecorateFolderNodes = 1
+vim.g.DevIconsEnableFoldersOpenClose = 1
+vim.g.DevIconsEnableFolderPatternMatching = 1
+
+
